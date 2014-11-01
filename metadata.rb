4bca405a01f83a9600fbcd98fc962191f5c9edc3
Gavin Reynolds <gsreynolds@gmail.com>
2014-11-01 17:39:09 +0000

    Bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'gsreynolds@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures FlexGet'
 long_description 'Installs/Configures FlexGet'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'python'
 depends 'cron'
